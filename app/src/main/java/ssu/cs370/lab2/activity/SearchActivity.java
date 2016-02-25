8e019922814c52eb852e0d6175eb0e570b723bec
Nathan <lavasurge@gmail.com>
2016-02-24 18:03:18 -0800

    Lab complete

--- app/src/main/java/ssu/cs370/lab2/activity/SearchActivity.java
+++ app/src/main/java/ssu/cs370/lab2/activity/SearchActivity.java
@@ -2,13 +2,21 @@ package ssu.cs370.lab2.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import ssu.cs370.lab2.R;
+import ssu.cs370.lab2.model.RecipeModel;
 
 public class SearchActivity extends AppCompatActivity {
 
     private EditText searchEditText;
+    private Button searchButton;
+    private TextView recipeNameTextView;
+    private TextView recipeCuisineTextView;
+    private TextView recipeDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,45 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         searchEditText = (EditText)findViewById(R.id.searchEditText);
+        searchButton = (Button)findViewById(R.id.searchButton);
+        recipeNameTextView = (TextView)findViewById(R.id.recipeNameTextView);
+        recipeCuisineTextView = (TextView)findViewById(R.id.recipeCuisineTextView);
+        recipeDescriptionTextView = (TextView)findViewById(R.id.recipeDescriptionTextView);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecipeModel ClickRecipe = new RecipeModel();
+                ClickRecipe = getMockRecipeData();
+
+                String mockName = ClickRecipe.getRecipeName();
+                recipeNameTextView.setText(mockName);
+                String mockCuisine = ClickRecipe.getRecipeCuisine();
+                recipeCuisineTextView.setText(mockCuisine);
+                String mockDesc = ClickRecipe.getRecipeDescription();
+                recipeDescriptionTextView.setText(mockDesc);
+
+
+
+
+            }
+        });
     }
+
+    private RecipeModel getMockRecipeData() {
+        RecipeModel mockRecipe = new RecipeModel();
+        mockRecipe.setRecipeName("Chicken Teriakyi");
+        mockRecipe.setRecipeDescription("Great basic Chicken Teriyaki recipe. Better than the bottled stuff.");
+        mockRecipe.setRecipeCuisine("Japanese");
+        mockRecipe.setRecipeCategory("Main Dish");
+        mockRecipe.setRecipePrimaryIngredient("Chicken");
+        mockRecipe.setRecipeReviewCount(34);
+        mockRecipe.setRecipeStarRating(4.5);
+        mockRecipe.setRecipeImageURL("http://redirect.bigoven.com/pics/rs/640/chicken-teriyaki-10.jpg");
+
+        return mockRecipe;
+    }
+
+
+
 }
--- app/src/main/java/ssu/cs370/lab2/model/RecipeModel.java
+++ app/src/main/java/ssu/cs370/lab2/model/RecipeModel.java
@@ -1,7 +1,7 @@
 package ssu.cs370.lab2.model;
 
 public class RecipeModel {
-
+// Name
     private String recipeName;
 
     public String getRecipeName() {
@@ -10,6 +10,83 @@ public class RecipeModel {
 
     public void setRecipeName(String recipeName) {
         this.recipeName = recipeName;
+    }
+
+//Desc
+    private String recipeDescription;
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
+    }
+
+//Cuisine
+    private String recipeCuisine;
+
+    public String getRecipeCuisine() {
+        return recipeCuisine;
+    }
+
+    public void setRecipeCuisine(String recipeCuisine) {
+        this.recipeCuisine = recipeCuisine;
+    }
+
+//Category
+    private String recipeCategory;
+
+    public String getRecipeCategory() {
+        return recipeCategory;
+    }
+
+    public void setRecipeCategory(String recipeCategory) {
+        this.recipeCategory = recipeCategory;
+    }
+
+//PrimIngredient
+    private String recipePrimaryIngredient;
+
+    public String getRecipePrimaryIngredient() {
+        return recipePrimaryIngredient;
+    }
+
+    public void setRecipePrimaryIngredient(String recipePrimaryIngredient) {
+        this.recipePrimaryIngredient = recipePrimaryIngredient;
+    }
+
+//Review #
+    private double recipeReviewCount;
+
+    public double getRecipeReviewCount() {
+        return recipeReviewCount;
+    }
+
+    public void setRecipeReviewCount(double recipeReviewCount) {
+        this.recipeReviewCount = recipeReviewCount;
+    }
+
+//Rating #
+    private double recipeStarRating;
+
+    public double getRecipeStarRating() {
+        return recipeStarRating;
+    }
+
+    public void setRecipeStarRating(double recipeStarRating) {
+        this.recipeStarRating = recipeStarRating;
+    }
+
+//Image URL
+    private String recipeImageURL;
+
+    public String getRecipeImageURL() {
+        return recipeImageURL;
+    }
+
+    public void setRecipeImageURL(String recipeImageURL) {
+        this.recipeImageURL = recipeImageURL;
     }
 
 }
